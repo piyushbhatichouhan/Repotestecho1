58dc5dfc7070d8993bb1009bb1ac56b7d133977c
piyush-bhati-chouhan <piyushbhatichouhan@gmail.com>
2026-07-20 12:16:04 +0530

    new code to test environment

--- index.js
+++ index.js
@@ -1,50 +1,34 @@
-// Load environment variables from a .env file
-
 const express = require("express");
-const cors = require("cors");
-const helmet = require("helmet");
+const axios = require("axios");
 
 const app = express();
-const PORT = 3001;
 
-// --- Global Middleware ---
-app.use(helmet()); // Sets security HTTP headers
-app.use(cors()); // Enables Cross-Origin Resource Sharing
-app.use(express.json()); // Parses incoming requests with JSON payloads
-app.use(express.urlencoded({ extended: true })); // Parses URL-encoded payloads
+const PORT = process.env.PORT || 3001;
 
-app.get("/health", (req, res) => {
-  res.status(200).json({
-    status: "ok",
-  });
-});
+const API_URL = process.env.API_URL;
 
-// --- Sample Route ---
 app.get("/", (req, res) => {
-  res.status(200).json({
+  res.json({
     success: true,
-    message: "Welcome to the Express API Boilerplate!",
+    message: "Echo deployment test",
+    api: API_URL,
   });
 });
 
-// --- 404 Route Handler ---
-app.use((req, res, next) => {
-  const error = new Error("Resource Not Found");
-  error.status = 404;
-  next(error);
+app.get("/joke", async (req, res) => {
+  try {
+    const response = await axios.get(API_URL);
+
+    res.json(response.data);
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      error: err.message,
+    });
+  }
 });
 
-// --- Centralized Error Handling Middleware ---
-app.use((error, req, res, next) => {
-  res.status(error.status || 500).json({
-    success: false,
-    error: {
-      message: error.message || "Internal Server Error",
-    },
-  });
-});
-
-// --- Start Server ---
 app.listen(PORT, () => {
-  console.log(`Server successfully running on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
+  console.log(`API_URL = ${API_URL}`);
 });
