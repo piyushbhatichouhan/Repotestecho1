a446fde27d0f7839ac55a9dcd321f1c55bc39c9b
piyushbhatichouhan <piyushbhatichouhan@gmail.com>
2026-07-28 09:59:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const PORT = 3001;
 // Serve all static files from the 'public' folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
