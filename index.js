334fdca280935d4b7d0ef7f95fac41899cca7f52
piyushbhatichouhan <piyushbhatichouhan@gmail.com>
2026-08-06 16:00:08 +0530

    ghg

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.CONTAINER_PORT || 3000;
 
 // Serve all static files from the 'public' folder
 app.use(express.static(path.join(__dirname, "public")));
